6635c1362f77b9e2c59641bee7fb8f428a2e2984
alenchess <alana.ste05@gmail.com>
2025-11-08 08:10:08 +0000

    лр 6

--- lab6/src/client.c
+++ lab6/src/client.c
@@ -19,33 +19,41 @@ struct Server {
   int port;
 };
 
+// ждем ответ от сервера
 void *WaitForResponse(void *args) {
-  int sck = *((int *)args);
-  char response[sizeof(uint64_t)];
+  int sck = *((int *)args);  // Получаем дескриптор сокета
+  char response[sizeof(uint64_t)];  // Буфер для ответа
+  
+  // Получаем данные от сервера
   if (recv(sck, response, sizeof(response), 0) < 0) {
     fprintf(stderr, "Recieve failed\n");
     exit(1);
   }
+  
   close(sck);
+  
+
   uint64_t *ans = malloc(sizeof(uint64_t));
-  memcpy(ans, response, sizeof(uint64_t));
+  memcpy(ans, response, sizeof(uint64_t)); //копируем, потом возвращаем указатель
+  
   return (void *)ans;
 }
 
-
 int main(int argc, char **argv) {
-  uint64_t k = -1;
+  uint64_t k = -1; 
   uint64_t mod = -1;
-  char servers[255] = {'\0'}; // TODO: explain why 255
+  char servers[255] = {'\0'}; //путь к серверу
   FILE *a;
 
   while (true) {
     int current_optind = optind ? optind : 1;
 
-    static struct option options[] = {{"k", required_argument, 0, 0},
-                                      {"mod", required_argument, 0, 0},
-                                      {"servers", required_argument, 0, 0},
-                                      {0, 0, 0, 0}};
+    static struct option options[] = {
+      {"k", required_argument, 0, 0},
+      {"mod", required_argument, 0, 0},
+      {"servers", required_argument, 0, 0},
+      {0, 0, 0, 0}
+    };
 
     int option_index = 0;
     int c = getopt_long(argc, argv, "", options, &option_index);
@@ -58,7 +66,6 @@ int main(int argc, char **argv) {
       switch (option_index) {
       case 0:
         ConvertStringToUI64(optarg, &k);
-        // TODO: your code here
         if (k <= 0) {
           printf("k must be a positive number!\n");
           return 1;
@@ -66,14 +73,13 @@ int main(int argc, char **argv) {
         break;
       case 1:
         ConvertStringToUI64(optarg, &mod);
-        // TODO: your code here
         if (mod <= 0) {
           printf("mod must be a positive number!\n");
           return 1;
         }
         break;
-      case 2:
-        // TODO: your code here
+      case 2: 
+        // Проверяем, что файл существует и доступен для чтения
         a = fopen(optarg, "r");
         if (a == NULL) {
           printf("Failed to open file!\n");
@@ -102,38 +108,49 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // TODO: for one server here, rewrite with servers from file
+  // колво серверов, нач размер
   unsigned int servers_num = 0;
-  int size = 10;
+  int size = 10; 
   struct Server *to = malloc(sizeof(struct Server) * size);
+  
   a = fopen(servers, "r");
-  char *cadress;
   char tmp[255] = {'\0'};
+  
+  // читаем файл построчно
   while (!feof(a)) {
+    // увеличиваем если надо
     if (servers_num == size) {
       size = size + 10;
       to = realloc(to, sizeof(struct Server) * size);
     }
+    
     fscanf(a, "%s\n", tmp);
+    
+    // парсим строку "IP:PORT"
     char *marker = strtok(tmp, ":");
     memcpy(to[servers_num].ip, tmp, sizeof(tmp));
     to[servers_num].port = atoi(strtok(NULL, ":"));
     servers_num++;
   }
+
   if (to[servers_num - 1].port == 0)
     servers_num--;
+    
   fclose(a);
+  
   if (servers_num > k) {
     printf("Number of servers is bigger than k\n");
     servers_num = k;
     printf("Only %llu servers will be used\n", k);
   }
-  // TODO: delete this and parallel work between servers
-  // TODO: work continiously, rewrite to make parallel
-  int ars = k / servers_num;
+
+  int ars = k / servers_num; 
   int left = k % servers_num;
-  int sck[servers_num];
+  
+  int sck[servers_num];        // массивы сокетов и потоков
   pthread_t threads[servers_num];
+
+  // подключение к серверам и отправка задач
   for (int i = 0; i < servers_num; i++) {
     struct hostent *hostname = gethostbyname(to[i].ip);
     if (hostname == NULL) {
@@ -141,27 +158,30 @@ int main(int argc, char **argv) {
       exit(1);
     }
 
+    // настройка структуры адреса сервера
     struct sockaddr_in server;
-    server.sin_family = AF_INET;
+    server.sin_family = AF_INET; 
     server.sin_port = htons(to[i].port);
     server.sin_addr.s_addr = *((unsigned long *)hostname->h_addr_list[0]);
 
+    // Создание TCP-сокета
     sck[i] = socket(AF_INET, SOCK_STREAM, 0);
     if (sck[i] < 0) {
       fprintf(stderr, "Socket creation failed!\n");
       exit(1);
     }
 
+    // Подключение к серверу
     if (connect(sck[i], (struct sockaddr *)&server, sizeof(server)) < 0) {
       fprintf(stderr, "Connection failed\n");
       exit(1);
     }
 
-    // TODO: for one server
-    // parallel between servers
+    // вычисление диапазона чисел для текущего сервера
     uint64_t begin = ars * i + (left < i ? left : i) + 1;
     uint64_t end = ars * (i + 1) + (left < i + 1 ? left : i + 1) + 1;
 
+    // формирование задачи: [begin, end, mod]
     char task[sizeof(uint64_t) * 3];
     memcpy(task, &begin, sizeof(uint64_t));
     memcpy(task + sizeof(uint64_t), &end, sizeof(uint64_t));
@@ -176,23 +196,24 @@ int main(int argc, char **argv) {
     *sck_copy = sck[i];
     if (pthread_create(&threads[i], NULL, (void *)WaitForResponse,
                        (void *)sck_copy)) {
-      printf("Error: pthread_create failed!\n");
+      printf("Error: pthread_create failed!\n"); //тут поток который ждет ответ
       return 1;
     }
   }
 
-    // TODO: from one server
-    // unite results
-  uint64_t answer = 1;
-  uint64_t response = 0;
+  // Сбор и объединение результатов от всех серверов
+  uint64_t answer = 1; 
   for (int i = 0; i < servers_num; i++) {
     uint64_t *response;
+    
     pthread_join(threads[i], (void **)&response);
-    answer = MultModulo(*response, answer, mod);
-    free(response);
+    answer = MultModulo(*response, answer, mod); // ждем результаты и бъединяем результат с текущим (умножение по модулю)
+    
+    free(response);  // Освобождаем память
   }
 
   printf("Factorial %llu by mod %llu = %llu\n", k, mod, answer);
+  
   free(to);
 
   return 0;
